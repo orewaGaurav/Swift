7e23065a5fb3f650d07f2a4ac3654df46a5473e2
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-06-09 11:41:40 +0530

    hlw

---
+++ function.swift
@@ -0,0 +1,23 @@
+import UIKit
+var name = "Subject"
+print(name)
+
+//function
+func firstFunction(){
+    print("Hello Gaurav")
+}
+firstFunction()
+
+func Try(name:String){
+    print(name)
+    
+}
+Try(name:"Walter White")
+Try(name:"Hesignberg")
+
+func Add(firstNum:Int,to secondNum:Int)-> Int{
+    let sum = firstNum + secondNum
+    return sum
+}
+Add(firstNum: 34, to: 35)
+Add(firstNum:69,to:31 )
